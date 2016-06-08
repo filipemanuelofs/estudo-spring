2ec10ea321b64453d48583f761dd3199a65669a7
filipe.sousa <filipe.sousa@cnj.jus.br>
2016-06-07 21:59:04 -0300

    Utilizando mensagem de sucesso com Thymeleaf e alteração do método salvar()

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.TituloRepository;
@@ -23,9 +24,15 @@ public class TituloController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String salvar(Titulo titulo) {
-		tituloRepository.save(titulo);
-		System.out.println(titulo.getDescricao());
-		return PAGINA;
+	public ModelAndView salvar(Titulo titulo) {
+		ModelAndView mv = new ModelAndView(PAGINA);
+		try {
+			tituloRepository.save(titulo);			
+			mv.addObject("mensagem", "Título cadastrado com sucesso.");
+		} catch (Exception e) {
+			mv.addObject("mensagem", "Não foi possível cadastrar o título informado.");
+			e.printStackTrace();
+		}
+		return mv;
 	}
 }
